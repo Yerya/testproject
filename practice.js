c7073220db09f42250f354b732832868eb6051bb
Erya <ermolay9@gmail.com>
2023-12-07 19:09:50 +0200

    first commit

---
+++ practice.js
@@ -0,0 +1,25 @@
+"use strict";
+
+const obj = {
+  name: "Alex",
+  age: 23,
+  isMarried: true,
+};
+
+obj[3] = 3;
+// console.log(obj.age);
+console.log(obj);
+
+const emptyArr = [];
+
+console.log(emptyArr);
+
+console.log(typeof null);
+
+console.log(4 + "5");
+
+let a = 5;
+let b = 5;
+a--, b++;
+console.log(a, b);
+console.log(2 + 2 * 2 !== "6");
