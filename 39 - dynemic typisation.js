21ab4851d5ab60bfc683f86a1a64172be155ff11
Erya <ermolay9@gmail.com>
2024-01-02 00:48:41 +0200

    41 - dynemic stuff

--- 39 - dynemic typisation.js
+++ 39 - dynemic typisation.js
@@ -8,7 +8,7 @@
 console.log(typeof String(null)); //string
 console.log(typeof String(4)); //string
 //2) конкатинация(сложение строк или сложение строки с чем то), что то складываем со строкой.
-//со сложением со строкой будет строка
+//со сложением со строкой будет строка всегда
 
 console.log(typeof (null + ""));
 const a = null;
@@ -61,3 +61,70 @@ if (switcher) {
 console.log(typeof Boolean("4"));
 
 console.log(typeof !!"4");
+
+//ПРЕОБРАЗОВАНИЕ К СТРОКЕ
+
+// Если один из операндов является строкой, другой операнд также преобразуется в строку.
+const value = "The answer is: " + 42;
+console.log(value); // "The answer is: 42"
+
+//ПРЕОБРАЗОВАНИЕ К ЧИСЛУ
+
+// JavaScript приводит значения к числу в арифметических операциях (кроме сложения, если один из операндов — строка),
+// при использовании унарного плюса и в операторах сравнения.
+const stringToNumber = "5" - "2";
+console.log(stringToNumber); // 3 (number)
+
+const unaryPlus = +"5";
+console.log(unaryPlus); // 5
+
+const comparison = "5" > 2;
+console.log(comparison); // true
+
+//ПРЕОБРАЗОВАНИЕ К БУЛЕАН
+
+// В логических операциях, таких как && и ||, значения приводятся к логическому типу.
+const truthyOrFalsy = "hello" || "";
+console.log(truthyOrFalsy); // "hello"
+
+const truthyAndFalsy = "hello" && "";
+console.log(truthyAndFalsy); // ""
+
+//ПРЕОБРАЗОВАНИЕ ОБЪЕКТОВ В ПРИМИТИВЫ
+
+// Когда объект (включая массивы и функции) участвует в операции, приводящей к примитивному типу,
+// вызываются методы valueOf() и toString() объекта для получения примитивного значения.
+const arrayToString = [1, 2, 3].toString();
+console.log(arrayToString); // "1,2,3"
+
+const objectToString = { a: 1 }.toString();
+console.log(objectToString); // "[object Object]"
+
+//ОСОБЫЙ СЛУЧАЙ: МАСИИВЫ И ЧИСЛА
+
+// При сложении массива и числа, массив сначала преобразуется в строку,
+// а затем происходит конкатенация со строковым представлением числа.
+const arrayPlusNumber = [1, 2, 3] + 4;
+console.log(arrayPlusNumber); // "1,2,34"
+
+/* Преобразование к числу */
+
+// undefined преобразуется в NaN
+console.log(Number(undefined)); // NaN
+
+// Если строка не может быть преобразована в допустимое число, результатом будет NaN
+console.log(Number("hello")); // NaN
+console.log(Number("false")); // NaN
+
+//пустая строка преобразуется в 0
+console.log(Number("")); // 0
+
+// null преобразуется в 0
+console.log(Number(null)); // 0
+
+// true и false преобразуются в 1 и 0 соответственно
+console.log(Number(true)); // 1
+console.log(Number(false)); // 0
+
+// Строка преобразуется в число, которое она представляет
+console.log(Number("123")); // 123
