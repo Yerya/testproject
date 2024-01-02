d25126864b923f92bd1669eb00102944599e17a2
Erya <ermolay9@gmail.com>
2024-01-02 22:34:12 +0200

    43 - actions with dom elements

--- 43 - elements/elements/js/script.js
+++ 43 - elements/elements/js/script.js
@@ -45,3 +45,74 @@ hearts.forEach((item, i) => {
 
 const oneHeart = document.querySelector(".heart");
 console.log(oneHeart);
+
+const wrapper = document.querySelector(".wrapper");
+//////////////////////////////////////////////
+// console.dir(box);
+
+box.style.backgroundColor = "blue";
+box.style.width = "500px";
+
+btns[1].style.borderRadius = "100%";
+
+circles[0].style.backgroundColor = "green";
+
+//ниже чтобы несколько присваивать сразу свойсты
+
+box.style.cssText = "background-color: green; width: 500px"; //записываем текст, который хотим в инлайн стили поместить
+//полезно если например какие то параметры рассчитывать динамически
+//box.style.cssText = `background-color: ${green}; width: ${num}px`;
+
+//чтобы сразу над несколькими элементами произвести действия, можно использовать
+//цикл, for, for of какой нибудь, foreach
+
+hearts.forEach((item) => {
+  item.style.display = "flex";
+});
+
+for (let i = 0; i < btns.length; i++) {
+  btns[i].style.backgroundColor = "pink";
+}
+
+//ниже создали элемент и он существует только
+//внутри js
+const div = document.createElement("div");
+//этот метод ниже используется редко
+const text = document.createTextNode("Тут был я");
+
+//Элементы существуют в js, чтобы стилизовать обычно
+//лучше модифицировать css классы (добавить/удалить/проверять на содержание/переключать/определение количества примененных классов)
+div.classList.add("black"); //добавляес класс
+//Ниже добавляем див в конец body.
+document.body.append(div); //в самый конец добавляем див
+
+// wrapper.append(div); в конец добавляет
+wrapper.prepend(div);
+
+hearts[0].before(div); //добавляет перед первым элементов сердечек див
+hearts[0].after(div); //добавляет после первого элемента сердечек див
+
+circles[1].remove(); //удаляет второй кружок
+
+//замена элементов
+
+hearts[1].replaceWith(circles[0]); //второе сердце заменили первым кружком
+
+//редактирование HTML кода
+
+div.innerHTML = "<h1>Hello Man</h1>"; //используется с html структурой
+
+//Благодаря этому можно динамически изменять структуру сайта
+
+// div.textContent = "Hello"; //работает уже только с текстом, без тега.
+//сделано для безопасности, чтобы если что то пользователь вводил,
+//не сломал бы верстку.
+
+//как вставить кусочек html кода
+
+div.insertAdjacentHTML("beforeend", "<h2>Hi</h2>"); //2 аргумента, первый спец. слово, второй -
+//тот html, который хотим вставить.
+//beforebegin - перед элементом
+//afterbegin - после элемента
+//beforeend - перед внутри элемента
+//afterend - после внутри элемента
